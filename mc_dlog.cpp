d1ae4d5849eda24d4f88f3a392c5212401afc2e3
WrightTony <37187667+WrightTony@users.noreply.github.com>
2018-11-15 17:04:55 +0000

    Add files via upload

--- mc_dlog.cpp
+++ mc_dlog.cpp
@@ -10,59 +10,69 @@ int n = 10;
 
 //calculate the square root of a given number
 //code reference: https://www.youtube.com/watch?v=znuNWLiBX2A
-unsigned long sqrt(unsigned long n) {
-	for (i = 0.01; i*i < n; i = i + 0.01);
-	return i;
+int sqrt(int number) {
+
+	double error = 0.00001; //define the precision of your result
+    double s = number;
+
+    while ((s - number / s) > error) //loop until precision satisfied 
+    {
+        s = (s + number / s) / 2;
+    }
+    return s;
+
 }
 
 //calculate the power of a given number to a given number
 //code reference: https://www.youtube.com/watch?v=lNdI_7f4d68
-unsigned long pow(unsigned long n, int p) {
+int pow(int n, int p) {
 	int temp;
 	int c = 1;
 
-	for (i = 1; i <= p; i++) {
+	for (int i = 1; i <= p; i++) {
 		temp=n;
 		c=c*temp;
+		//mod here when you know what going on
 	}
 
 	return c;
 }
 
 //calculate g to the r mod n
-unsigned long sqrMod(int g, int r, int n) {
+int sqrMod(int g, int r, int n) {
 	return pow(g, r) % n;
 }
 
 //calculate a*g mod n
-unsigned long timesMod(int a, int g, int r) {
-	return a * g % r;
+int timesMod(int a, int g, int r) {
+	return (a * g) % r;
 }
 
-unsigned long orderOfG() {
+int orderOfG(int g, int n) {
 
 	//create a hash table
 	HashTable Ord;
-
-	int r = 0;
 
 	//repeat for sqrt n times
 	for (int i = 0; i < sqrt(n); i++) {
 
 		//pick a random number between 1 and n-1 
-		std::default_random_engine generator;
-		std::uniform_int_distribution<int> distribution(1, n - 1);
-		r = distribution(generator);
+		default_random_engine  e(static_cast<unsigned int>(time(0)));
+		uniform_int_distribution<int> d2(0, (n - 1));
+
+		int r = d2(e);
+
+		int y = sqrMod(g, r, n);
 
 		//condition: If y = gr mod n is already a key in table Ord
-		if (findInTable(Ord, sqrMod(g, r, n)) {
+		if (Ord.isInTable(y)) {
 			//duplicate key found
 			
-			if (r - Ord[hash_function(y)] > 0) {
-				return r - Ord[hash_function(y)];
+			if (r - Ord.getValue(y) > 0) {
+				return r - Ord.getValue(y);
 			}
-			else if (Ord[hash_function(y)] - r > 0) {
-				return Ord[hash_function(y)] - r;
+			else if(Ord.getValue(y) - r > 0){ 
+				return Ord.getValue(y) - r;
 			}
 
 		}
@@ -73,66 +83,113 @@ unsigned long orderOfG() {
 		}
 	}
 
+
 	//duplicate key not found, assume order of g is n-1
-
-
+	return n-1;
 }
 
-unsigned long findDLog() {
+int findDLog(int g, int a, int n) {
 
 	//create two hash tables
 	HashTable A;
 	HashTable B;
 
-	int r = 0;
+	int y;
 
 	for (int i = 0; i < sqrt(n); i++) {
 
 		//pick a random number r between 0 and n-1
-		std::default_random_engine generator;
-		std::uniform_int_distribution<int> distribution(1, n - 1);
-		r = distribution(generator);
+		default_random_engine  e(static_cast<unsigned int>(time(0)));
+		uniform_int_distribution<int> d2(0, (n - 1));
 
-		if (findInTable(B, timesMod(a, g, n)) {
-			return B[hash_function(y)] - r;
+		int r = d2(e);
+
+		cout << "r: " << r << endl;
+
+		y = timesMod(a, g, n);
+
+		cout << "y: " << y << endl;
+
+		if (B.isInTable(y)) {
+			if (B.getValue(y) - r > 0){
+				return B.getValue(y) - r;
+			}
+			else{
+				return r - B.getValue(y);
+			}
 		}
 		else {
 			//store r under key y in table A
+			A.insert(y,r);
 		}
 
 		//pick a random number r between 0 and n-1
-		std::default_random_engine generator;
-		std::uniform_int_distribution<int> distribution(1, n - 1);
-		r = distribution(generator);
+		int r2 = d2(e);
 
-		if (findInTable(A, timesMod(a, g, n)) {
-			return r - A[hash_function(y)];
+		y = sqrMod(a, g, n);
+
+		if (A.isInTable(y)) {
+
+			if (r2 - A.getValue(y) > 0){
+				return r2 - A.getValue(y);
+			}
+			else if (A.getValue(y) - r2 > 0){
+				return A.getValue(y) - r2;
+			}
+			
 		}
 		else {
 			//store r under key y in table B
+			B.insert(y, r);
 		}
-			
-			
+
+		cout << "End of loop rotation " << i << endl;
 	}
+
+	//if not found then return zero
+	return 0;
+
 }
 
 
-int main() {
+int main() { //int argc, char** argv
+
+	int g, a, n;
 
 	//get three inputs g, a and n
 	cin >> g;
 	cin >> a;
 	cin >> n;
 
-	output = findDLog() % orderOfG();
-
-	if (output < 0) {
-		output += orderOfG();
+	/*
+	if (argc != 4) { //if there arent enough arguments
+		cout << "Please enter the correct number of arguments" << endl;
+		return 1;
 	}
+	else{
+*/
+		int order, log;
 
-	cout << "x = " << output << endl;
-	
-	return 0;
+		order = orderOfG(g,n);
+
+		cout << "Order of G: " << order << endl;
+
+		log = findDLog(g,a,n);
+
+		cout << "Log: " << log << endl;
+
+		int output = log % order;
+
+		if (output < 0) {
+			output += order;
+		}
+
+		cout << "x = " << output << endl;
+		
+		return 0;
+	//}
+
+
 }
 
 
